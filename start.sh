84cba197d74e9143d3dfed4043b88923a81dfff9
Isaac <isaac.vazquez@iteso.mx>
2024-11-12 21:22:44 -0600

    modified start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env bash
-exec gunicorn main:fastapi_app \
+#!/bin/bash
+pip install -r requirements.txt
+exec gunicorn main:app \
     --worker-class uvicorn.workers.UvicornWorker \
     --bind 0.0.0.0:$PORT \
     --workers 4 \
