9e0fd42c84513eaebd8f7a175938bdd7cc6ae1d3
Isaac <isaac.vazquez@iteso.mx>
2024-11-12 18:37:21 -0600

    Prepare app for Render deployment

--- main.py
+++ main.py
@@ -37,8 +37,8 @@ class RegisterRequest(BaseModel):
 #### Authentication ####
 
 # JWT config and Password Hashing
-SECRET_KEY = "20022BA"
-ALGORITHM = "HS256"
+SECRET_KEY = os.getenv("SECRET_KEY", "20022BA")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 # OAuth2 Scheme
